b4f08615d0c71eab2364e175e668cad25e54196f
Vladimir <34865131+Stacker007@users.noreply.github.com>
2025-05-06 18:10:08 +0300

    Update Role.java
    return getRole in Role

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -39,7 +39,10 @@ public class Role implements GrantedAuthority {
     public void setId(Long id) {
         this.id = id;
     }
-
+    
+    public String getRole() {
+        return role;
+    }
 
     public void setRole(String role) {
         this.role = role;
